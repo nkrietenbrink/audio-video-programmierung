3851605eaed49dd7a87e9ab79083fdeeb83d9801
nkrietenbrink <n.krietenbrink@gmail.com>
2018-10-08 12:34:38 +0200

    A5

---
+++ A5/script.js
@@ -0,0 +1,7 @@
+document.body.addEventListener('mousemove', function(e){
+    console.log(e.clientX);
+    console.log(e.clientY);
+
+    console.log(window.innerWidth);
+    console.log(window.innerHeight);
+});
